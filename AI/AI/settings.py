2d4b6f5efba3adfb372574af6bc01344757e2b2c
Tidings2024 <147610880+Tidings2024@users.noreply.github.com>
2023-10-12 19:38:27 +0200

    vercel

--- AI/AI/settings.py
+++ AI/AI/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ohs)%8)(x*p3+u!hck%@0_%imvxast@$oxqz@dri6)8*ub-ejd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [.vercel.app]
 
 
 # Application definition
